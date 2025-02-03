48129da5ca0e82f177be3f31e2de33863961fbcb
Ariles Fellah <ariles.fellah937@gmail.com>
2025-02-03 22:56:10 +0100

    Fin de l'exo2

--- TP_1/tp-combine.js
+++ TP_1/tp-combine.js
@@ -13,41 +13,51 @@ function getEmployees() {
 module.exports = {
   // 1) Renvoyer un nouveau tableau qui ne garde que les personnes dont le nom se termine par un "e"
   B1() {
-    // Implémentation ici
+
+    return getEmployees().filter(prsn => prsn.name.endsWith("e"));
+  
   },
 
   // 2) Trouver la première personne dont l'âge est inférieur à 30
   B2() {
-    // Implémentation ici
+
+    return getEmployees().find((prsn) => prsn.age < 30);
+
   },
 
   // 3) Renvoyer l'index où se trouve "Jeremy"
   B3() {
-    // Implémentation ici
+    return getEmployees().findIndex((prsn) => prsn.name == "Jeremy");
   },
 
   // 4) Trier le tableau par âge des personnes
   B4() {
-    // Implémentation ici
+    return getEmployees().sort((a, b) => Number(a.age) - Number(b.age));
   },
 
   // 5) Ajouter Kasimu à la fin du tableau, il a 38 ans, et renvoyer le tableau modifié
   B5() {
-    // Implémentation ici
+    let prns = getEmployees();
+    prns.push({ name: "Kasimu", age: 38})
+    return prns;
   },
 
   // 6) Renvoyer la longueur du tableau
   B6() {
-    // Implémentation ici
+    return getEmployees().length;
   },
 
   // 7) Remplacer le "o" de Theo par un "a", et renvoyer le tableau modifié
   B7() {
-    // Implémentation ici
+    let prns = getEmployees();
+    let index = prns.findIndex((prns) => prns.name === "Theo");
+    prns[index].name = prns[index].name.replace("o", "a");
+    console.log(prns[index].name);
+    return prns;
   },
 
   // 8) Renvoyer un tableau qui ne contient que les personnes ayant au moins un "e" dans leur nom
   B8() {
-    // Implémentation ici
+    return getEmployees().filter((prns) => prns.name.includes("e"));
   },
 };
--- TP_1/tp-object.js
+++ TP_1/tp-object.js
@@ -17,39 +17,46 @@ function getObject() {
 module.exports = {
   // 1) Renvoyer la propriété description
   Q1() {
-    // Implémentation ici
+    return getObject()["description"];
   },
 
   // 2) Renvoyer la propriété city
   Q2() {
-    // Implémentation ici
+    return getObject().address.city;
   },
 
   // 3) Renvoyer les clés de l'objet sous forme de tableau
   Q3() {
-    // Implémentation ici
+    return Object.keys(getObject());
   },
 
   // 4) --- Supprimée ---
 
   // 5) Ajouter la propriété email à myObject et renvoyer l'objet modifié
   Q5() {
-    // Implémentation ici
+    let objet = getObject();
+    objet.email = email;
+    return objet;
   },
 
   // 6) Ajouter une propriété dynamique étant le résultat de la concaténation des chaines hello et world et dont la valeur est true
   // renvoyer l'objet modifié
   Q6() {
-    // Implémentation ici
+    let objet = getObject();
+    objet["hello" + "world"] = true;
+    //Sinon on peux faire avec un concat : "Hello".concat("World")
+    return objet;
   },
 
   // 7) Supprimer la propriété isBestObject et renvoyer l'objet
   Q7() {
-    // Implémentation ici
+    let objet = getObject();
+    delete(objet.isBestObject);
+    return objet;
   },
 
   // 8) Vérifier que la propriété description existe dans l'objet, renvoyer un booléen
   Q8() {
-    // Implémentation ici
+    return getObject().hasOwnProperty("description");
   },
 };
