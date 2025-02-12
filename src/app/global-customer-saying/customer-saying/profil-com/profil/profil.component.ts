917a9a2fc91398c65d8ec91941db7041b62123a8
Ariles Fellah <ariles.fellah937@gmail.com>
2025-02-12 14:53:14 +0100

    fin du tp2

--- src/app/global-customer-saying/customer-saying/profil-com/profil/profil.component.ts
+++ src/app/global-customer-saying/customer-saying/profil-com/profil/profil.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Customer } from '../../../global-customer-saying.component';
+import { ProfilDescComponent } from './profil-desc/profil-desc.component';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [],
+  imports: [ProfilDescComponent],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss'
 })
