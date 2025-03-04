756fa42a02ea209cd550b84c7c5f8344ada82930
Fyromi <100370988+Fyromi@users.noreply.github.com>
2025-03-04 10:38:51 +0100

    Update user.component.ts

--- src/app/header/user/user.component.ts
+++ src/app/header/user/user.component.ts
@@ -11,7 +11,7 @@ import { UserLogoComponent } from './user-logo/user-logo.component';
 })
 export class UserComponent {
   
-  name: string = "Thomas";
+  name: string = "PauPau";
   initial: string = this.name[0];
 
 }
