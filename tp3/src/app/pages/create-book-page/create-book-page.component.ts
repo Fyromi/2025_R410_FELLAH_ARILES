03987d61e7b95715f6dd06361bc73d5af8d72eae
Ariles Fellah <ariles.fellah937@gmail.com>
2025-03-04 13:30:34 +0100

    Fin de partie 1

--- tp3/src/app/pages/create-book-page/create-book-page.component.ts
+++ tp3/src/app/pages/create-book-page/create-book-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, MinLengthValidator, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BooksInMemoryService } from '../../services/book-inmemory.service';
+import { Book } from '../../models/book';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-book-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-book-page.component.html',
   styleUrl: './create-book-page.component.css',
 })
-export class CreateBookPageComponent {}
+export class CreateBookPageComponent {
+
+  private readonly serviceBook = inject(BooksInMemoryService)
+
+  constructor(private readonly router: Router){}
+
+  bookForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]),
+    author: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(255)])
+  })
+  
+  onSubmit(){
+    if(this.bookForm.valid)
+      this.serviceBook.createBook(this.bookForm.value as Book)
+      this.router.navigate(['/'])
+  }
+  
+}
